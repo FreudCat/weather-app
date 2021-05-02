2b2ac2bfcf9e3aa144fb0ec4d3a768d2ded1eb4e
= <anguyen031@gmail.com>
2021-05-01 21:37:42 -0700

    Reorganize sections for index.html

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,25 @@
-
 const baseURL = "https://api.openweathermap.org/data/2.5/weather?zip=" //have to have the https:// in front of the url else it will append to the current local host and give you an error. 
-//api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={API key}
+let apiKey;
+
 let d = new Date()//gets current date and time 
-console.log(d);
 let newDate = (d.getMonth() + 1) + '/' + d.getDate() + '/' + d.getFullYear();
-console.log(newDate);
-let apiKey; 
+
 const postBtn = document.getElementById("post-info-btn");
 
+//variables used to hold user input and weather data 
 let zipcode = document.getElementById("zip");
 let feelings = document.getElementById("feelings");
 let content = document.getElementById("content");
 let temp = document.getElementById("temp");
+
+//variables used to update the DOM
 const dateDiv = document.getElementById("date");
 const tempDiv = document.getElementById("temp");
 const contentDiv = document.getElementById("content");
-const zipDiv = document.getElementById("userZip"); 
+const zipDiv = document.getElementById("user-zip"); 
 
 postBtn.addEventListener("click", function (e) {
+  if (zipcode.value && feelings.value) {
   e.preventDefault();
   console.log(zipcode.value);
   console.log(feelings.value);
@@ -32,8 +34,11 @@ postBtn.addEventListener("click", function (e) {
         }) //this function will post data that we got from web api query. Takes a URL and an object (web api data). There should also be a POST on the server side with the same url/route. 
     })
     .then(function () {
-      showData();  // now we will GET the data that was posted and use it to update the DOM. 
+      showData();  // chaining another promise to GET the data that was posted and use it to update the DOM. 
     }); 
+  } else {
+
+  }
 });
 
 const getWeather = async (baseURL, zipcode, apiKey) => { //Query a web API - pass base open weather url, zipcode from input, and apikey to openweather to get the information. Use async to let the function know we are going to write asynchronous code 
@@ -68,7 +73,7 @@ const postData = async (url, data) => {
   }
 }
 
-const showData = async () => { //Data has already been posted, now it needs to be retrieve to update the DOM
+const showData = async () => { //Data has already been posted, now it needs to be retrieved to update the DOM
   try {
     let fetchApiInfo = await fetch("/userEntry"); //this corresponds with a GET request on the server-side. Note the fetch and GET needs to use the same url. 
     let finalData = await fetchApiInfo.json();
